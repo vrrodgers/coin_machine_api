ef6dd5058bf2476405a65a7bce3b6039871cc9f5
vrrodgers <trulymissvicky1982@gmail.com>
2020-03-07 13:09:40 -0600

    implemented mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,17 +83,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  host = 'https://vicky-coin-machine-api.herokuapp.com/' #replace with your own url
-  config.action_mailer.default_url_options = { host: host }
-
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
-  }
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'https://vicky-coin-machine-api.herokuapp.com/',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
 end
