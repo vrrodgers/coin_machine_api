bbbd3185af3473dd20c2b116fa2e99cfbe0ecf5d
vrrodgers <trulymissvicky1982@gmail.com>
2020-03-07 13:16:47 -0600

    implemented mailgun env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'https://vicky-coin-machine-api.herokuapp.com/',
+  :domain         => ENV['DOMAIN'],
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
